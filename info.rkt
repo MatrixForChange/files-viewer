b7ffc5d594ba33090d944111138e3f4b4381045b
syntacticlosure <1227847322@qq.com>
2018-07-04 16:55:48 +0800

    g

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define authors '("jiangqi"))
 (define version "0.00.08")
 (define package-content-state '(built "6.12"))
-(define deps '("base" "gui-lib" "drracket"))
+(define deps '("base" "gui-lib" "drracket" "rackunit-lib"))
 (define drracket-tool-names (list "file-viewer"))
 (define drracket-tools (list (list "tool.rkt")))
 (define pkg-desc "file manager for racket") 
