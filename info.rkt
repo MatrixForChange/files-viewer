fdac895ce44b433e126776fe4215c850033a6596
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2018-07-09 22:05:55 +0100

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -5,5 +5,9 @@
 (define package-content-state '(built "6.12"))
 (define deps '("base" "gui-lib" "drracket" "rackunit-lib"))
 (define drracket-tool-names (list "file-viewer"))
+(define drracket-tool-icons  '(#f))
+
+(define scribblings '(("scribblings/files-viewer.scrbl" () (tool) "files-viewer")))
+
 (define drracket-tools (list (list "tool.rkt")))
 (define pkg-desc "file manager for racket")
