306e6b6a1e848f1a4097736b3e616dc843eef0ab
syntacticlosure <1227847322@qq.com>
2018-08-20 09:20:17 +0800

    fix dependency problem

--- info.rkt
+++ info.rkt
@@ -4,7 +4,8 @@
 (define version "0.00.42")
 (define package-content-state '(built "7.0"))
 (define deps '("base" "gui-lib" "drracket" "rackunit-lib" "scheme-lib"
-                      "compatibility-lib" "scribble-lib"))
+                      "compatibility-lib" "scribble-lib"
+                      "pict-lib"))
 (define drracket-tool-names (list "file-viewer"))
 (define drracket-tool-icons  '(#f))
 
