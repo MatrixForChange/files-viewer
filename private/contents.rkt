a4902713221c48e19b315d4e467e30ce53fbd7e2
Syntacticlosure <1227847322@qq.com>
2019-07-21 15:34:55 +0800

    fix gitignore template

--- private/contents.rkt
+++ private/contents.rkt
@@ -2,7 +2,6 @@
 (provide (all-defined-out))
 
 (define CONTENT-GITIGNORE (string-append
-                           "*.rkt\n"
                            "compiled/\n"
                            "/doc/\n"
                            "*.[0-9]\n"
